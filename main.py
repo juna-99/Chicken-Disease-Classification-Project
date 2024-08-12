69396c2a3d3e5478fd23cb3719288868117b82d1
afrina <afrinaghafar99@gmail.com>
2024-08-12 16:49:46 +0800

    training

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from src.cnnClassifier.pipeline.stage_01_data_ingestion import (
 from src.cnnClassifier.pipeline.stage_02_prepare_base_model import (
     PrepareBaseModelTrainingPipeline,
 )
+from src.cnnClassifier.pipeline.stage_03_training import (
+    ModelTrainingPipeline,
+)
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -29,3 +32,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Training"
+if __name__ == "__main__":
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
